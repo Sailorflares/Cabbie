3b21649cb1e65a16b86d1a5c56d52881031139e7
vaidehijoshi <vaidehi.sj@gmail.com>
2014-12-09 12:09:25 -0500

    adds initialze in aplpication rb

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,21 @@ Bundler.require(*Rails.groups)
 module Cabbie
   class Application < Rails::Application
 
-    config.serve_static_assets = true
+    # config.serve_static_assets = true
+
+    initializer 'setup_asset_pipeline', :group => :all  do |app|
+      # We don't want the default of everything that isn't js or css, because it pulls too many things in
+      app.config.assets.precompile.shift
+
+      # Explicitly register the extensions we are interested in compiling
+      app.config.assets.precompile.push(Proc.new do |path|
+        File.extname(path).in? [
+          '.html', '.erb', '.haml',                 # Templates
+          '.png',  '.gif', '.jpg', '.jpeg',         # Images
+          '.eot',  '.otf', '.svc', '.woff', '.ttf', # Fonts
+        ]
+      end)
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
